8feac67bf220c55e4bbb3c359138ef9304925daf
Tidatep <55011307+Tidatep@users.noreply.github.com>
2019-09-19 16:17:22 +0700

    Add files via upload

---
+++ Exercise4_Tidatep_P.py
@@ -0,0 +1,8 @@
+FE = 67.5
+GB = 55.4
+ICS = 51.3
+CP = 43
+print("Foundation English :",FE)
+print("General Business :",GB)
+print("Introduction to Computer Systems :",ICS)
+print("Computer Programming :",CP)
